0f8ad9038aa441c469b35b6f3075c688f7d0bc13
jimhotchkiss <jimbohtochkiss@yahoo.com>
2019-06-18 18:55:24 -0700

    Added show to controller and form_for

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,8 @@ class ListsController < ApplicationController
   end 
 
   def show
-    
+    @list = List.find(params[:id])
   end 
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   
   root 'lists#index'
 
+
+
 end
